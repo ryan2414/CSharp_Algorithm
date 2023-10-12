1c070443b6e4cbe3d148d3184a7cb170f0661792
이승재 <leeseungjae@jerry.local>
2023-10-12 21:52:45 +0900

    Init

---
+++ Algorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // 입력
+                
+                // 로직
+                // 렌더링
+            }
+        }
+    }
+}
