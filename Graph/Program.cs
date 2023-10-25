9c3c6613ebc63b0d593bdba0be664716f870bf55
이승재 <leeseungjae@jerry.local>
2023-10-25 22:01:36 +0900

    그래프 생성

---
+++ Graph/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Graph;
+
+class Graph
+{
+    int[,] adj = new int[6, 6]
+    {
+        { 0, 1, 0, 1, 0, 0},
+        { 1, 0, 1, 1, 0, 0},
+        { 0, 1, 0, 0, 0, 0},
+        { 1, 1, 0, 0, 1, 0},
+        { 0, 0, 0, 1, 0, 1},
+        { 0, 0, 0, 0, 1, 0},
+    };
+
+    List<int>[] adj2 = new List<int>[]
+    {
+        new List<int>() { 1, 3 },
+        new List<int>() { 0, 2, 3},
+        new List<int>() { 1 },
+        new List<int>() { 0, 1, 4 },
+        new List<int>() { 3, 5 },
+        new List<int>() { 5 },
+    };
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // DFS (Depth First Search 깊이 우선 탐색 )
+        // BFS (Breadth First Search 너비 우선 탐색 )
+    }
+}
+
---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace StackAndQueue;
+
+class Program
+{
+    // 선형 자료구조 -> 자료가 일렬로
+
+    // 스택 :  LIFO(후입선출 Last In First Out)
+    // 큐 : FIFO(선입선출 First In First Out)
+     
+    static void Main(string[] args)
+    {
+        Stack<int> stack = new Stack<int>();
+
+        stack.Push(101);
+        stack.Push(102);
+        stack.Push(103);
+        stack.Push(104);
+        stack.Push(105);
+
+        int data = stack.Pop();
+        int dat2 = stack.Peek();
+
+        Queue<int> queue = new Queue<int>();
+
+        queue.Enqueue(101);
+        queue.Enqueue(102);
+        queue.Enqueue(103);
+        queue.Enqueue(104);
+        queue.Enqueue(105);
+
+        int _data = queue.Dequeue();
+        int _data2 = queue.Peek();
+    }
+}
+
