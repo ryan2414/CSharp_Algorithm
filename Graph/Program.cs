42617a8a57efcf3387df53093b230d89160c0081
ryan2414 <jackpar@nate.com>
2023-11-02 21:24:21 +0900

    DFS 공부

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Graph;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Graph;
 
 class Graph
 {
@@ -7,8 +9,8 @@ class Graph
         { 0, 1, 0, 1, 0, 0},
         { 1, 0, 1, 1, 0, 0},
         { 0, 1, 0, 0, 0, 0},
-        { 1, 1, 0, 0, 1, 0},
-        { 0, 0, 0, 1, 0, 1},
+        { 1, 1, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0, 1},
         { 0, 0, 0, 0, 1, 0},
     };
 
@@ -17,10 +19,62 @@ class Graph
         new List<int>() { 1, 3 },
         new List<int>() { 0, 2, 3},
         new List<int>() { 1 },
-        new List<int>() { 0, 1, 4 },
-        new List<int>() { 3, 5 },
+        new List<int>() { 0, 1 },
+        new List<int>() { 5 },
         new List<int>() { 5 },
     };
+
+    bool[] visited = new bool[6];
+    // 1) 우선 now부터 방문하고,
+    // 2) now와 연결된 정점들을 하나씩 확인해, [아직 미발견(미방문)] 방문한다.
+    public void DFS(int now)
+    {
+        Console.WriteLine(now);
+        visited[now] = true; // 1) 우선 now부터 방문하고,
+
+        for (int next = 0; next < adj.GetLength(0); next++)
+        {
+            if (adj[now, next] == 0) // 연결되어 있지 않으면 스킵
+            {
+                continue;
+            }
+            if (visited[next]) // 이미 방문했으면 스킵
+            {
+                continue;
+            }
+
+            DFS(next);
+        }
+
+    }
+
+    public void DFS2(int now)
+    {
+        Console.WriteLine(now);
+        visited[now] = true; // 1) 우선 now부터 방문하고,
+
+        foreach (int next in adj2[now])
+        {
+            if (visited[next]) // 이미 방문했으면 스킵
+            {
+                continue;
+            }
+            DFS(next);
+
+        }
+    }
+
+    public void SearchAll()
+    {
+        visited = new bool[6];
+        for (int now = 0; now < 6; now++)
+        {
+            if (visited[now] == false)
+            {
+                DFS(now);
+            }
+        }
+    }
 }
 
 class Program
@@ -29,6 +83,8 @@ class Program
     {
         // DFS (Depth First Search 깊이 우선 탐색 )
         // BFS (Breadth First Search 너비 우선 탐색 )
+        Graph graph = new Graph();
+        graph.SearchAll();
     }
 }
 
